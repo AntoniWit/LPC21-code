4383bb1592d2bf7e4de5772958cbdef1a8686ad4
AntoniWit <antek12345w567@gmail.com>
2026-05-20 10:39:19 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "led.h"
+#include "keyboard.h"
+#include "timer.h"
+
+int main()
+{
+	LedInit();
+	InitTimer0Match0(250000);
+  while(1)
+  {
+		WaitOnTimer0Match0();
+		LedStepRight();
+		
+	}
+	
+	
+}
+//git test
